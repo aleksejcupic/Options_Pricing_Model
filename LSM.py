9e8c1babc393a40c32838ecde1422db10522ac37
Aleksej Cupic <82075265+aleksejcupic@users.noreply.github.com>
2022-04-04 17:34:35 -0400

    Initial Commit

---
+++ LSM.py
@@ -0,0 +1,7 @@
+# Abstract: An implementation of the American style Option Pricing Model
+# using the Least Squares Method for Monte Carlo Simulation from 
+# Longstaff, Schwartz, Valuing American Options by Simulation (2001)
+
+# The method is defined in their research. We implement it in Python
+
+# By: Komi Alasse, Gio Canales, Aleksej Cupic 
