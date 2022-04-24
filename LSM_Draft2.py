a08693325a3dfee834a9d0743a91f4af241e7b73
Komi Alasse <alasse@bc.edu>
2022-04-24 16:27:57 -0400

    "updated LSM"

--- LSM_Draft2.py
+++ LSM_Draft2.py
@@ -3,10 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import math
-import scipy
+from scipy.misc import derivative
 
 N = 10 # number of time steps, number of discretization points, time points that the option can be exercised
-M = 200 # number of paths of the underlying asset
+M = 11 # number of paths of the underlying asset
 T = 1 # time from 'now' to expiration of option in years 
 dt = T/N # time between each time step 
 r = 0.06 # risk free rate (%)
@@ -25,9 +25,8 @@ for i in range(1, N + 1):
     TABLE[:, i] = TABLE[:, i -1] + dS
 
 print(TABLE)
-
-
-def f(X, n):
+n = 10
+def f(X):
     return X ** n * numpy.exp(-X)
 
 # df = pd.DataFrame(TABLE)
@@ -39,18 +38,17 @@ k = 1.1
 
 # discounting the payoff
 df = pd.DataFrame(TABLE)
-for i in range(1):
-    Y = pd.DataFrame()
-    #(k - df[N]).map(lambda v: max(v, 0))
-    for i in range(0, M):
+for i in range(N):
+    Y = (k - df[N]).map(lambda v: max(v, 0))
+    """for i in range(0, M):
         price = TABLE[i][N] - k
         if price > 0:
             Y.append(price)
         else:
-            Y.append(0)
+            Y.append(0)"""
     # discount = numpy.exp(-mu * 1)
     # Y = Y * discount
-    X = TABLE[N-1]
+    X = df[N-i]
     ITM = X < k
     X = X[ITM]
     Y = Y[ITM]
@@ -59,7 +57,8 @@ for i in range(1):
     poly = pd.DataFrame(index=X.index)
     n = 10
     for i in range(0,n):
-        poly[i] = numpy.exp(-X/2) * numpy.exp(X) / math.factorial(i) * scipy.derivative(f(X,i), X, dx=1e-6, n=i)
+        n = i
+        poly[i] = numpy.exp(-X/2) * numpy.exp(X) / math.factorial(i) * derivative(f, X, dx=1e-6, n=i, order=15)
     # poly[0] = numpy.exp(-X/2)
     # poly[1] = numpy.exp(-X/2) * (1 - X)
     # poly[2] = numpy.exp(-X/2) * (1 - (2*X) + (X**2 / 2))
@@ -71,20 +70,20 @@ for i in range(1):
 
     # determining whether continuing is better or not 
     continuation = (poly * coef).sum(axis=1)
-    exercise = (k - TABLE[N-1][ITM])
+    exercise = (k - TABLE[N-i][ITM])
     x = numpy.linspace(.5, 1.5, 100)
     y = 0
     for i in range(0, n):
         y += poly[i] * coef[i]
     continued = continuation > exercise
-    continued = continued.reindex(TABLE.index).fillna(True)
+    continued = continued.reindex(df.index).fillna(True)
     print(continued)
 
-    plt.figure(figsize=(10,10))
+    """plt.figure(figsize=(10,10))
     plt.plot(x,y, linestyle=":", color="blue")
 
     plt.scatter(X, Y, color="red", label="Y (discounted exercise later)")
 
     plt.scatter(X, continuation, label="continuation", marker="x", color="blue")
     plt.scatter(X, exercise, label="exercise now", marker="+", color="green")
-    plt.legend()
+    plt.legend()"""
