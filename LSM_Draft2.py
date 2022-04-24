c5848dd3be29bd21c22f309afb160c8dba8d76cc
Aleksej Cupic <82075265+aleksejcupic@users.noreply.github.com>
2022-04-24 17:22:08 -0400

    changes

--- LSM_Draft2.py
+++ LSM_Draft2.py
@@ -7,17 +7,21 @@ from scipy.misc import derivative
 
 N = 10 # number of time steps, number of discretization points, time points that the option can be exercised
 M = 11 # number of paths of the underlying asset
+BF = 10 # number of basis functions used
 T = 1 # time from 'now' to expiration of option in years 
 dt = T/N # time between each time step 
-r = 0.06 # risk free rate (%)
-S = 1 # stock price 
-sigma = 0.3
-mu = 0.06
 
+PRICE = 1 # stock price 
+STRIKE_PRICE = 1.1
+
+# initialize table with stock price
 TABLE = numpy.zeros((M, N + 1))
-TABLE[:, 0] = S
+TABLE[:, 0] = PRICE
 
-# filling the table standard Brownian motion
+# Filling the table with stochastic diferential equation 
+r = 0.06 # risk free rate (%)
+sigma = 0.3 # constant for eq
+# Z is standard Brownian Motion 
 for i in range(1, N + 1):
     dZ = numpy.random.normal(size=M) * numpy.sqrt(dt) 
     # see eq 7 in longstaff-shcwartz 2001
@@ -25,43 +29,39 @@ for i in range(1, N + 1):
     TABLE[:, i] = TABLE[:, i -1] + dS
 
 print(TABLE)
-n = 10
+n = BF
+
+# part of laguerre weighted polynomial function 
 def f(X):
     return X ** n * numpy.exp(-X)
 
-# df = pd.DataFrame(TABLE)
-k = 1.1
-# df.transpose().plot(color="red", alpha=0.3)
-# plt.legend([])
-# plt.plot([0,N], [k, k], label="strke price")
-# plt.show()
+# creation of dataframe data type in order to 
+# use with stats model functions 
+TABLE_DF = pd.DataFrame(TABLE)
 
-# discounting the payoff
-df = pd.DataFrame(TABLE)
-for i in range(N):
-    Y = (k - df[N]).map(lambda v: max(v, 0))
-    """for i in range(0, M):
-        price = TABLE[i][N] - k
-        if price > 0:
-            Y.append(price)
-        else:
-            Y.append(0)"""
-    # discount = numpy.exp(-mu * 1)
-    # Y = Y * discount
-    X = df[N-i]
-    ITM = X < k
-    X = X[ITM]
-    Y = Y[ITM]
+# loop for backward induction on each time step from N to 0
+# this is for put options
+for i in range(0, N - 1):
+    # Y is the array of profit at time step N - i 
+    Y = (STRIKE_PRICE - TABLE_DF[N - i]).map(lambda v: max(v, 0))
+    # X is the price of each path at time N - i
+    X = TABLE_DF[N - i - 1]
+    # IN_THE_MONEY is an array at time N - i that shows T/F
+    # for whether the option is in the money
+    IN_THE_MONEY = X < STRIKE_PRICE
+    X = X[IN_THE_MONEY]
+    Y = Y[IN_THE_MONEY]
 
     # laguerre polynomials 
+    # poly is also a dataframe data type 
     poly = pd.DataFrame(index=X.index)
-    n = 10
-    for i in range(0,n):
+    n = BF
+
+    # see eq 5 in longstaff-schwartz 2001
+    # polys are recreated at each iteration because indicies may change
+    for i in range(0, n):
         n = i
         poly[i] = numpy.exp(-X/2) * numpy.exp(X) / math.factorial(i) * derivative(f, X, dx=1e-6, n=i, order=15)
-    # poly[0] = numpy.exp(-X/2)
-    # poly[1] = numpy.exp(-X/2) * (1 - X)
-    # poly[2] = numpy.exp(-X/2) * (1 - (2*X) + (X**2 / 2))
 
     # stats model 
     model = sm.OLS(Y, poly)
@@ -70,15 +70,75 @@ for i in range(N):
 
     # determining whether continuing is better or not 
     continuation = (poly * coef).sum(axis=1)
-    exercise = (k - TABLE[N-i][ITM])
+    exercise = (STRIKE_PRICE - TABLE_DF[N - i][IN_THE_MONEY])
     x = numpy.linspace(.5, 1.5, 100)
     y = 0
-    for i in range(0, n):
-        y += poly[i] * coef[i]
+    
+    # see eq 6 in Longstaff, Schwartz 2001
+    for p in range(0, n):
+        y += poly[p] * coef[p]
     continued = continuation > exercise
-    continued = continued.reindex(df.index).fillna(True)
+    continued = continued.reindex(TABLE_DF.index).fillna(True)
     print(continued)
+    Y[continued] = 0
 
+    # plotting the graphs
+    """plt.figure(figsize=(10,10))
+    plt.plot(x,y, linestyle=":", color="blue")
+
+    plt.scatter(X, Y, color="red", label="Y (discounted exercise later)")
+
+    plt.scatter(X, continuation, label="continuation", marker="x", color="blue")
+    plt.scatter(X, exercise, label="exercise now", marker="+", color="green")
+    plt.legend()"""
+
+
+
+
+# loop for backward induction on each time step from N to 0
+# this is for call options
+for i in range(0, N - 1):
+    # Y is the array of profit at time step N - i 
+    Y = (TABLE_DF[N - i] - STRIKE_PRICE).map(lambda v: max(v, 0))
+    # X is the price of each path at time N - i
+    X = TABLE_DF[N - i - 1]
+    # IN_THE_MONEY is an array at time N - i that shows T/F
+    # for whether the option is in the money
+    IN_THE_MONEY = X > STRIKE_PRICE
+    X = X[IN_THE_MONEY]
+    Y = Y[IN_THE_MONEY]
+
+    # laguerre polynomials 
+    # poly is also a dataframe data type 
+    poly = pd.DataFrame(index=X.index)
+    n = BF
+
+    # see eq 5 in longstaff-schwartz 2001
+    # polys are recreated at each iteration because indicies may change
+    for i in range(0, n):
+        n = i
+        poly[i] = numpy.exp(-X/2) * numpy.exp(X) / math.factorial(i) * derivative(f, X, dx=1e-6, n=i, order=15)
+
+    # stats model 
+    model = sm.OLS(Y, poly)
+    res = model.fit()
+    coef = res.params
+
+    # determining whether continuing is better or not 
+    continuation = (poly * coef).sum(axis=1)
+    exercise = (TABLE_DF[N - i][IN_THE_MONEY] - STRIKE_PRICE)
+    x = numpy.linspace(.5, 1.5, 100)
+    y = 0
+    
+    # see eq 6 in Longstaff, Schwartz 2001
+    for p in range(0, n):
+        y += poly[p] * coef[p]
+    continued = continuation < exercise
+    continued = continued.reindex(TABLE_DF.index).fillna(True)
+    print(continued)
+    Y[continued] = 0
+
+    # plotting the graphs
     """plt.figure(figsize=(10,10))
     plt.plot(x,y, linestyle=":", color="blue")
 
