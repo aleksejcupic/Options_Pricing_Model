643ac15c9401a4b26bb84b6d7d395785e9a30fd3
Aleksej Cupic <82075265+aleksejcupic@users.noreply.github.com>
2022-05-04 17:58:39 -0400

    graph changes

--- Final/PutsChebyshev.py
+++ Final/PutsChebyshev.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 
 # PUTS USING CHEBYSHEV POLYNOMIALS 
-N = 100 # number of time steps, number of discretization points, time points that the option can be exercised
-M = 100 # number of paths of the underlying asset
+N = 10 # number of time steps, number of discretization points, time points that the option can be exercised
+M = 20 # number of paths of the underlying asset
 BF = 20 # number of basis functions used, pandas cannot handle more than 20
 T = 1 # time from 'now' to expiration of option in years 
 dt = T/N # time between each time step 
 
 PRICE = 1
-STRIKE_PRICE = 1
+STRIKE_PRICE = 1.08
 
 # initialize table with stock price
 TABLE = np.zeros((M, N + 1))
@@ -39,7 +39,7 @@ plt.show()
 
 # determining in-the-money 
 Y = (STRIKE_PRICE - TABLE_DF[N]).map(lambda v: max(v, 0))
-X = TABLE_DF[N-1]
+X = TABLE_DF[N-2]
 IN_THE_MONEY = X < STRIKE_PRICE
 X = X[IN_THE_MONEY]
 Y = Y[IN_THE_MONEY]
@@ -66,7 +66,7 @@ coef = res.params
 
 # continuing / exercise 
 continuation = (poly * coef).sum(axis=1)
-exercise = (STRIKE_PRICE - TABLE_DF[N-1][IN_THE_MONEY])
+exercise = (STRIKE_PRICE - TABLE_DF[N-2][IN_THE_MONEY])
 
 # x scale 
 x = np.linspace(.5, 1.5, 100)
@@ -78,7 +78,7 @@ y = 1 * coef[0] + x * coef[1] + ((2 * (x ** 2)) - 1) * coef[2] #+ ((4 * (X ** 3)
 # plotting 
 plt.figure(figsize=(10,10))
 plt.plot(x,y, linestyle=":", color="blue")
-plt.xlabel(f"price at {N-1}", fontdict=None, labelpad=None, loc=None)
+plt.xlabel(f"price at {N-2}", fontdict=None, labelpad=None, loc=None)
 plt.ylabel(f"amount profit at {N}", fontdict=None, labelpad=None, loc=None)
 plt.title(f'Puts with starting price: {PRICE} and strike price: {STRIKE_PRICE}')
 plt.scatter(X, Y, marker="o",color="orange", label="Y (discounted exercise later, actual value that was gotten later)")
--- Final/PutsLaguerre.py
+++ Final/PutsLaguerre.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 
 # PUTS USING LAGUERRE POLYNOMIALS
-N = 1000 # number of time steps, number of discretization points, time points that the option can be exercised
-M = 1000 # number of paths of the underlying asset
+N = 10 # number of time steps, number of discretization points, time points that the option can be exercised
+M = 20 # number of paths of the underlying asset
 BF = 20 # number of basis functions used, pandas cannot handle more than 20
 T = 1 # time from 'now' to expiration of option in years 
 dt = T/N # time between each time step 
 
-PRICE = 100
-STRIKE_PRICE = 108
+PRICE = 1
+STRIKE_PRICE = 1.08
 
 # initialize table with stock price
 TABLE = np.zeros((M, N + 1))
@@ -42,14 +42,14 @@ plt.show()
 
 # determining in-the-money 
 Y = (STRIKE_PRICE - TABLE_DF[N]).map(lambda v: max(v, 0))
-X = TABLE_DF[N-200]
+X = TABLE_DF[N-2]
 IN_THE_MONEY = X < STRIKE_PRICE
 X = X[IN_THE_MONEY]
 Y = Y[IN_THE_MONEY]
 
 # scatter plot
 plt.scatter(X, Y)
-plt.xlabel(f"price at {N-200}", fontdict=None, labelpad=None, loc=None)
+plt.xlabel(f"price at {N-2}", fontdict=None, labelpad=None, loc=None)
 plt.ylabel(f"amount profit at {N}", fontdict=None, labelpad=None, loc=None)
 plt.title(f'Puts with starting price: {PRICE} and strike price: {STRIKE_PRICE}')
 plt.show()
@@ -69,7 +69,7 @@ coef = res.params
 
 # continuing / exercise 
 continuation = (poly * coef).sum(axis=1)
-exercise = (STRIKE_PRICE - TABLE_DF[N-200][IN_THE_MONEY])
+exercise = (STRIKE_PRICE - TABLE_DF[N-2][IN_THE_MONEY])
 
 # x scale 
 x = np.linspace(.5, 1.5, 100)
@@ -81,7 +81,7 @@ y = np.exp(-x/2) * coef[0] + np.exp(-x/2) * (1 - x) * coef[1] + np.exp(-x/2) * (
 # plotting 
 plt.figure(figsize=(10,10))
 plt.plot(x,y, linestyle=":", color="blue")
-plt.xlabel(f"price at {N-200}", fontdict=None, labelpad=None, loc=None)
+plt.xlabel(f"price at {N-2}", fontdict=None, labelpad=None, loc=None)
 plt.ylabel(f"amount profit at {N}", fontdict=None, labelpad=None, loc=None)
 plt.title(f'Puts with starting price: {PRICE} and strike price: {STRIKE_PRICE}')
 plt.scatter(X, Y, marker="o",color="orange", label="Y (discounted exercise later, actual value that was gotten later)")
